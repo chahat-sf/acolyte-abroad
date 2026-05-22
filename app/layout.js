3e67355f29a19f209bff86dfd5e9d1d5a77713b7
chahat-sf <chahat@startupflora.com>
2026-05-22 18:03:52 +0530

    add pixel to headers

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import {
   Plus_Jakarta_Sans,
   Just_Me_Again_Down_Here,
 } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const jakartaSans = Plus_Jakarta_Sans({
@@ -28,7 +29,32 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${jakartaSans.variable}  ${justMeAgain.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1484794056469592');
+fbq('track', 'PageView');`}
+        </Script>
+      </head>
+      <body className="min-h-full flex flex-col">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1484794056469592&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
